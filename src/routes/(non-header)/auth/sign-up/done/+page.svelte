560456f22ebeac8d4df831ad92685d9652cb6274
XIYO <bunny@xiyo.dev>
2025-05-23 14:50:02 +0900

    refactor: 회원가입 완료 화면에 필요 없는 타이머 제거

--- src/routes/(non-header)/auth/sign-up/done/+page.svelte
+++ src/routes/(non-header)/auth/sign-up/done/+page.svelte
@@ -1,32 +1,8 @@
-<script>
-  import { onMount } from 'svelte';
-  import { goto } from '$app/navigation';
-
-  let timer = $state(5);
-  onMount(() => {
-    const interval = setInterval(() => {
-      if (--timer <= 0) {
-        clearInterval(interval);
-        goto('/', { replaceState: true });
-      }
-    }, 1000);
-
-    return () => clearInterval(interval);
-  });
-</script>
-
-<svelte:head>
-  <noscript>
-    <meta content="5; url=/" http-equiv="refresh" />
-  </noscript>
-</svelte:head>
-
 <p class="text-center text-lg font-semibold mt-20">
   회원가입이 완료되었습니다.<br />
   이메일을 확인하여 인증을 진행해 주세요.<br />
-  {timer}초 후 홈으로 돌아갑니다.
 </p>
 
 <p class="text-center mt-4">
-  <a href="/" class="underline hover:text-gray-700">지금 바로 홈으로 이동하기</a>
+  <a href="/" class="underline hover:text-gray-700">홈으로 이동하기</a>
 </p>
